6faf31d43dbf330751ec4978648acb7497500a72
NACHOSWITHCHEESE <79577815+NACHOSWITHCHEESE@users.noreply.github.com>
2021-11-27 18:09:56 +1100

    Update gibbs.c

--- overflowing/gibbs.c
+++ overflowing/gibbs.c
@@ -1,41 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <signal.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include "asm.h"
 
-#define FLAGSIZE_MAX 64
+#define BUFFSIZE 64
+#define FLAGSIZE 64
 
-char flag[FLAGSIZE_MAX];
+void flag() {
+  char buf[FLAGSIZE];
+  FILE *f = fopen("flag.txt","r");
+  if (f == NULL) {
+    printf("Flag File is Missing. please contact an Admin if you are running this on the shell server.\n");
+    exit(0);
+  }
 
-void sigsegv_handler(int sig) {
-  fprintf(stderr, "%s\n", flag);
-  fflush(stderr);
-  exit(1);
+  fgets(buf,FLAGSIZE,f);
+  printf(buf);
 }
 
-void vuln(char *input){
-  char buf[128];
-  strcpy(buf, input);
+void vuln(){
+  char buf[BUFFSIZE];
+  gets(buf);
+
+  printf("Woah, were jumping to 0x%x !\n", get_return_address());
 }
 
 int main(int argc, char **argv){
-  
-  FILE *f = fopen("flag.txt","r");
-  if (f == NULL) {
-    printf("Flag File is Missing. Problem is Misconfigured, please contact an Admin if you are running this on the shell server.\n");
-    exit(0);
-  }
-  fgets(flag,FLAGSIZE_MAX,f);
-  signal(SIGSEGV, sigsegv_handler);
-  
+
+  setvbuf(stdout, NULL, _IONBF, 0);
   gid_t gid = getegid();
   setresgid(gid, gid, gid);
-  
-  if (argc > 1) {
-    vuln(argv[1]);
-    printf("You entered: %s", argv[1]);
-  }
-  else
-    printf("Please enter an argument next time\n");
+  puts("Give me a string and lets see what happens: ");
+  vuln();
   return 0;
 }
